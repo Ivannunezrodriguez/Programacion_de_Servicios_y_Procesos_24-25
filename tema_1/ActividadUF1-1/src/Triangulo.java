055794ca97fa6e2c94b88fd34a4b212fd870e201
Ivan Núñez Rodriguez <ivannr20@gmail.com>
2024-10-04 12:46:07 +0200

    1 clase

---
+++ tema_1/ActividadUF1-1/src/Triangulo.java
@@ -0,0 +1,17 @@
+public class Triangulo
+{
+    public static void main(String[] args) {
+        if (args.length==0){
+            System.out.println("se requiere un argumento");
+            return;
+        }
+        int filas=Integer.parseInt(args[0]);
+        for (int i = filas;i>1;i--){
+            for (int n =1; n <=1;n++){
+                System.out.println(n);
+            }
+
+            System.out.println();
+        }
+    }
+}
